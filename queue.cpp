2990e04a4236ec0d51615241ede0aef2d70a3c1e
Dang Khanh <dangkhanh1505@gmail.com>
2026-04-26 22:12:01 +0700

    feat: add player and queue data structures

---
+++ queue.cpp
@@ -0,0 +1,51 @@
+#include "queue.h"
+#include <iostream>
+#include <stdexcept>
+
+void MatchQueue::addPlayer(const Player& p) {
+    waitingList.push(p);
+}
+
+Player MatchQueue::popFront() {
+    if (waitingList.empty()) {
+        throw std::runtime_error("Hang doi trong!");
+    }
+    Player front = waitingList.top();
+    waitingList.pop();
+    return front;
+}
+
+int MatchQueue::getTopPlayers(Player* result, int n) const {
+    int count = 0;
+    std::priority_queue<Player, std::vector<Player>, CompareWait> temp = waitingList;
+
+    while (!temp.empty() && count < n) {
+        result[count] = temp.top();
+        temp.pop();
+        count++;
+    }
+
+    return count;
+}
+
+bool MatchQueue::isEmpty() const {
+    return waitingList.empty();
+}
+int MatchQueue::size() const {
+    return (int)waitingList.size();
+}
+
+void MatchQueue::printQueue() const {
+    std::cout << "=== HANG DOI HIEN TAI (" << waitingList.size() << " nguoi) ===\n";
+    std::priority_queue<Player, std::vector<Player>, CompareWait> temp = waitingList;
+
+    int stt = 1;
+    while (!temp.empty()) {
+        std::cout << stt << ". ";
+        printPlayer(temp.top());
+        temp.pop();
+        stt++;
+    }
+
+    std::cout << "=======================================\n";
+}
